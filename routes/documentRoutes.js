9afede900ebd192798ef9c11fe08a29c2a965c86
Divyanshu Patel <divu11480@gmail.com>
2026-01-08 17:51:27 +0530

    second commit

---
+++ routes/documentRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const upload = require("../config/multer");
+const protect = require("../middleware/auth");
+const {
+    uploadDocument, getDocuments, getDocumentsById, deleteDocument
+} = require("../controllers/documentController");
+
+const router = express.Router();
+
+router.post("/", protect, upload.single("file"), uploadDocument);
+router.get("/", protect, getDocuments);
+router.get("/:id", protect, getDocumentsById);
+router.delete("/:id", protect, deleteDocument);
+
+module.exports = router;
