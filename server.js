25573c20c1528dfcc80ea34ae0bf58fbfe2687ed
Divyanshu Patel <divu11480@gmail.com>
2026-02-06 13:47:38 +0530

    cors

--- server.js
+++ server.js
@@ -18,7 +18,12 @@ const path = require("path");
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["http://localhost:3000", 'https://ai-learning-assistant-frontend-two.vercel.app'],
+        credentials: true,
+    }
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
